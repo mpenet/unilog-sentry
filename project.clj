3177e9b10ae0d9af5d7e167d6ff77e693970d5dc
Max Penet <m@qbits.cc>
2017-05-11 14:29:32 +0200

    bump

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject cc.qbits/unilog-sentry "0.1.2"
+(defproject cc.qbits/unilog-sentry "0.1.3"
   :description ""
   :url "https://github.com/mpenet/unilog-sentry"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [spootnik/unilog "0.7.19"]
+                 [spootnik/unilog "0.7.20"]
                  [com.getsentry.raven/raven-logback "7.8.3"]]
   :source-paths ["src/clj"]
   :global-vars {*warn-on-reflection* true})

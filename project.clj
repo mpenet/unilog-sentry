55985a0ba299f1980c83df42907fb682070726b6
Sébastien Fievet <_@sebastien-fievet.fr>
2017-08-30 09:55:52 +0200

    Use stable clojure release

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/mpenet/unilog-sentry"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [spootnik/unilog "0.7.20"]
                  [io.sentry/sentry-logback "1.5.2"]]
   :global-vars {*warn-on-reflection* true})

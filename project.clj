701a221c9fcc978f4d488f72ae51759a0e9efa46
Sébastien Fievet <_@sebastien-fievet.fr>
2017-08-30 11:12:30 +0200

    Version 0.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/unilog-sentry "0.2.2-SNAPSHOT"
+(defproject cc.qbits/unilog-sentry "0.2.2"
   :description ""
   :url "https://github.com/mpenet/unilog-sentry"
   :license {:name "Eclipse Public License"

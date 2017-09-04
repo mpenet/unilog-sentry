91f6b85f5f5ad12b936ccdf28302bcb5b4ed87ed
Max Penet <m@qbits.cc>
2017-09-04 15:52:37 +0200

    lein pedantic + bump

--- project.clj
+++ project.clj
@@ -1,13 +1,16 @@
-(defproject cc.qbits/unilog-sentry "0.2.4-SNAPSHOT"
+(defproject cc.qbits/unilog-sentry "0.2.4"
   :description ""
   :url "https://github.com/mpenet/unilog-sentry"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [spootnik/unilog "0.7.20"]
-                 [io.sentry/sentry-clj "0.5.0"]
-                 [io.sentry/sentry-logback "1.5.2"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+                 [spootnik/unilog "0.7.21"]
+                 [io.sentry/sentry-clj "0.5.0"
+                  :exclusions [org.slf4j/slf4j-api com.fasterxml.jackson.core/jackson-core]]
+                 [io.sentry/sentry-logback "1.5.2"
+                  :exclusions [io.sentry/sentry org.slf4j/slf4j-api]]
                  [ring/ring-codec "1.0.1"]]
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
+  :pedantic? :abort
   :global-vars {*warn-on-reflection* true})
